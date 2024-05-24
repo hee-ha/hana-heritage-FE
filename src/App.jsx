b211aaca131b3146f8cab614c181e41a76031fa5
hyerimmy <hyerim2000@swu.ac.kr>
2024-05-24 23:13:21 +0900

    chore : 네비게이션바 수정 - 글씨 크기 키움 - 디자인 수정 - 예금, 적금 잘못된 것 수정

--- src/App.jsx
+++ src/App.jsx
@@ -4,37 +4,37 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Layout from "./components/Layout";
 import {
-  Deposit,
-  Home,
-  Login,
-  RetirementManagement,
-  Saving,
-  SimpleTransaction,
+	Deposit,
+	Home,
+	Login,
+	RetirementManagement,
+	Saving,
+	SimpleTransaction,
 } from "./pages";
 import Inheritance from "./pages/inheritance/Inheritance";
 import "./styles/globals.css";
 
 function App() {
-  return (
-    <RecoilRoot>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="deposit" element={<Deposit />} />
-            <Route path="savings" element={<Saving />} />
-            <Route path="simple-transaction" element={<SimpleTransaction />} />
-            <Route path="inheritance" element={<Inheritance />} />
-            <Route
-              path="retirement-management"
-              element={<RetirementManagement />}
-            />
-            <Route path="login" element={<Login />} />
-          </Route>
-        </Routes>
-      </Router>
-    </RecoilRoot>
-  );
+	return (
+		<RecoilRoot>
+			<Router>
+				<Routes>
+					<Route path="/" element={<Layout />}>
+						<Route index element={<Home />} />
+						<Route path="deposit" element={<Deposit />} />
+						<Route path="saving" element={<Saving />} />
+						<Route path="simple-transaction" element={<SimpleTransaction />} />
+						<Route path="inheritance" element={<Inheritance />} />
+						<Route
+							path="retirement-management"
+							element={<RetirementManagement />}
+						/>
+						<Route path="login" element={<Login />} />
+					</Route>
+				</Routes>
+			</Router>
+		</RecoilRoot>
+	);
 }
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,31 @@
 import React from "react";
 import Nav from "../Nav";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-  return (
-    <header className="bg-white shadow">
-      <div className="container mx-auto flex justify-between items-center py-4 px-6">
-        <div className="flex items-center">
-          <img
-            src="/png/Hana_Bank.png"
-            alt="하나은행 로고"
-            className="h-8 mr-2"
-          />
-        </div>
-        <Nav />
-      </div>
-    </header>
-  );
+	return (
+		<header className="bg-white shadow">
+			<div className="flex container mx-auto justify-between items-center sm:px-6 py-6 lg:px-8">
+				<div className="flex items-center">
+					<Link to="/">
+						<img
+							src="/png/Hana_Bank.png"
+							alt="하나은행 로고"
+							className="h-12 mr-2"
+						/>
+					</Link>
+					<Nav />
+				</div>
+				<Link
+					to="/login"
+					className="text-white bg-hanaRed  font-hana2 font-medium rounded-md px-3 py-2 text-3xl"
+					aria-current="page"
+				>
+					로그인
+				</Link>
+			</div>
+		</header>
+	);
 };
 
 export default Header;
