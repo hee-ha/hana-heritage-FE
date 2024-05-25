bfc2daae5791465b3fec3941a1bce714f40fc379
hyerimmy <hyerim2000@swu.ac.kr>
2024-05-26 00:24:48 +0900

    feat : 적금 상세 화면 페이지 및 라우터 추가

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
 import {
 	Deposit,
+	DepositDetail,
 	Home,
 	Login,
 	RetirementManagement,
@@ -22,6 +23,11 @@ function App() {
 					<Route path="/" element={<Layout />}>
 						<Route index element={<Home />} />
 						<Route path="deposit" element={<Deposit />} />
+						<Route path="deposit/detail" element={<DepositDetail />} />
+						<Route path="deposit/sign/1" element={<DepositDetail />} />
+						<Route path="deposit/sign/2" element={<DepositDetail />} />
+						<Route path="deposit/sign/3" element={<DepositDetail />} />
+						<Route path="deposit/sign/4" element={<DepositDetail />} />
 						<Route path="saving" element={<Saving />} />
 						<Route path="simple-transaction" element={<SimpleTransaction />} />
 						<Route path="inheritance" element={<Inheritance />} />
