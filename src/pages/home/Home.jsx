d045ef96e78cf1af17e6458e1ac841c947cd430d
JeongHeum Byun <quswjdgma83@gmail.com>
2024-05-27 18:40:39 +0900

    충돌해결

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Button from "../../components/common/Button/Button";
+import { Button } from "../../components/common";
 
 const Home = () => {
   return (
     <div className="bg-landing flex flex-col items-center justify-center space-y-5">
       <div>
-        <h1 className="text-white font-NotoSans font-semibold text-6xl z-10 animate-float">
+        <h1 className="text-white font-semibold text-6xl z-10 animate-float">
           시니어를 위한 하나은행 웹사이트에 오신것을 환영합니다!
         </h1>
       </div>
@@ -38,31 +38,27 @@ const Home = () => {
       </div>
       <div className="flex space-x-5">
         <Link to="/login">
-          <Button isDisabled={true} label={"로그인"} fontSize="text-5xl" />
+          <Button label={"로그인"} fontSize="text-5xl" />
           <Button
             isDisabled={true}
             label={"로그인"}
             fontSize="text-5xl"
             paddingY="py-2"
           />
-          <Button isDisabled={true} label={"로그인"} fontSize="text-5xl" />
-          <Button
-            isDisabled={true}
-            label={"로그인"}
-            fontSize="text-5xl"
-            font={"font-notoSans"}
-          />
-          <Button isDisabled={true} label={"로그인"} fontSize="text-4xl" />
-          <Button isDisabled={true} label={"로그인"} fontSize="text-3xl" />
-          <Button isDisabled={true} label={"로그인"} fontSize="text-2xl" />
-          <Button isDisabled={true} label={"로그인"} fontSize="text-1xl" />
+          {/* <Button label={"로그인"} fontSize="text-5xl" /> */}
+          <Button label={"계좌"} font="font-notoKR"></Button>
+          <Button label={"계좌"} font="font-noto"></Button>
+          <Button label={"로그인"} fontSize="text-4xl" />
+          <Button label={"로그인"} fontSize="text-3xl" />
+          <Button label={"로그인"} fontSize="text-2xl" />
+          <Button label={"로그인"} fontSize="text-1xl" />
 
-          <div class="relative h-11 w-full min-w-[200px]">
+          <div className="relative h-11 w-full min-w-[200px]">
             <input
               placeholder="Static"
-              class="peer h-full w-full border-b border-blue-gray-200 bg-transparent pt-4 pb-1.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border-blue-gray-200 focus:border-gray-900 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+              className="peer h-full w-full border-b border-blue-gray-200 bg-transparent pt-4 pb-1.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border-blue-gray-200 focus:border-gray-900 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
             />
-            <label class="after:content[' '] pointer-events-none absolute left-0  -top-2.5 flex h-full w-full select-none !overflow-visible truncate text-sm font-normal leading-tight text-gray-500 transition-all after:absolute after:-bottom-2.5 after:block after:w-full after:scale-x-0 after:border-b-2 after:border-gray-500 after:transition-transform after:duration-300 peer-placeholder-shown:leading-tight peer-placeholder-shown:text-blue-gray-500 peer-focus:text-sm peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:after:scale-x-100 peer-focus:after:border-gray-900 peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+            <label className="after:content[' '] pointer-events-none absolute left-0  -top-2.5 flex h-full w-full select-none !overflow-visible truncate text-sm font-normal leading-tight text-gray-500 transition-all after:absolute after:-bottom-2.5 after:block after:w-full after:scale-x-0 after:border-b-2 after:border-gray-500 after:transition-transform after:duration-300 peer-placeholder-shown:leading-tight peer-placeholder-shown:text-blue-gray-500 peer-focus:text-sm peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:after:scale-x-100 peer-focus:after:border-gray-900 peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
               Static
             </label>
           </div>
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -19,6 +19,7 @@ function Login() {
           로그인 화면으로 이동합니다
         </h1>
       ) : (
+<<<<<<< HEAD
         <div className="container mx-auto max-w-md p-10 mt-6 mb-6 bg-white rounded-xl shadow-xl">
           <h2 className="font-hana font-bold text-4xl mb-6 text-center">
             로그인
@@ -49,6 +50,70 @@ function Login() {
               </button>
             </div>
           </form>
+=======
+        <div className="flex flex-col items-center animate-fadeIn mt-5">
+          <img src="/png/Login.png" alt="로그인 로고" className="h-32 mr-2" />
+          <span className="font-hana2 font-semibold text-2xl whitespace-nowrap">
+            아이디 로그인
+          </span>
+          {/* 아이디 */}
+          <div className="mb-6">
+            <label
+              htmlFor="success"
+              className="block mb-2 text-sm font-medium text-green-700 dark:text-green-500"
+            >
+              Your name
+            </label>
+            <input
+              type="text"
+              id="success"
+              className="bg-green-50 border border-green-500 text-green-900 dark:text-green-400 placeholder-green-700 dark:placeholder-green-500 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-green-500"
+              placeholder="Success input"
+            />
+            {/* <p className="mt-2 text-sm text-green-600 dark:text-green-500">
+              <span className="font-medium">Well done!</span> Some success
+              message.
+            </p> */}
+          </div>
+
+          {/* 비밀번호 */}
+          <div className="mb-6">
+            <Button label={"로그인"}>하지마</Button>
+            <label
+              htmlFor="success"
+              className="block text-sm font-notoSans font-semibold dark:text-green-500"
+            >
+              Your name
+            </label>
+            <input
+              type="text"
+              id="success"
+              className="border border-green-500 text-hanaBlack-900 placeholder-green-700 dark:placeholder-green-500 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-3 dark:bg-gray-700 dark:border-green-500 shadow-md hover:shadow-lg duration-300 ease-in-out"
+              placeholder="Success input"
+            />
+
+            {/* <p className="mt-2 text-sm text-green-600 dark:text-green-500">
+              <span className="font-medium">Well done!</span> Some success
+              message.
+            </p> */}
+          </div>
+          {/* <p class="flex items-center gap-1 mt-2 font-sans text-sm antialiased font-normal leading-normal text-gray-700">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              class="w-4 h-4 -mt-px"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm8.706-1.442c1.146-.573 2.437.463 2.126 1.706l-.709 2.836.042-.02a.75.75 0 01.67 1.34l-.04.022c-1.147.573-2.438-.463-2.127-1.706l.71-2.836-.042.02a.75.75 0 11-.671-1.34l.041-.022zM12 9a.75.75 0 100-1.5.75.75 0 000 1.5z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+            Use at least 8 characters, one uppercase, one lowercase and one
+            number.
+          </p> */}
+>>>>>>> 3b08752 (chore: 사용하지않는 속성값 삭제(#14))
         </div>
       )}
     </div>
