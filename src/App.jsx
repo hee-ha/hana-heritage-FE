83a3253fad135e4948efa524a03e5a539a5f338f
JeongHeum Byun <quswjdgma83@gmail.com>
2024-05-27 18:38:54 +0900

    충돌해결

--- src/App.jsx
+++ src/App.jsx
@@ -12,14 +12,15 @@ import {
   RetirementManagement,
   Saving,
   SimpleTransaction,
+<<<<<<< HEAD
   Register,
   SignUpFull,
   Identify,
+=======
+>>>>>>> 3178d39 (chore: 충동해결(#14))
   AutoTransfer,
   TransferView,
-=======
   TransactionHistory,
->>>>>>> 5477fd9 (feat: 거래내역조회페이지 개발(#14))
 } from "./pages";
 import Inheritance from "./pages/inheritance/Inheritance";
 import "./styles/globals.css";
@@ -33,6 +34,7 @@ function App() {
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
             <Route path="deposit" element={<Deposit />} />
+<<<<<<< HEAD
             <Route path="savings" element={<Saving />} />
             <Route path="simple-transaction" element={<SimpleTransaction />} />
             <Route path="inheritance" element={<Inheritance />} />
@@ -53,6 +55,22 @@ function App() {
             <Route path="auto" element={<AutoTransfer />} />
             <Route path="transferView" element={<TransferView />} />
               
+=======
+            <Route path="deposit/detail" element={<DepositDetail />} />
+            <Route path="deposit/sign/1" element={<DepositSign1 />} />
+            <Route path="deposit/sign/2" element={<DepositDetail />} />
+            <Route path="deposit/sign/3" element={<DepositDetail />} />
+            <Route path="deposit/sign/4" element={<DepositDetail />} />
+            <Route path="saving" element={<Saving />} />
+            <Route path="simple-transaction" element={<SimpleTransaction />} />
+            <Route
+              path="simple-transaction/TransactionHistory"
+              element={<TransactionHistory />}
+            />
+            <Route path="inheritance" element={<Inheritance />} />
+            <Route path="auto" element={<AutoTransfer />} />
+            <Route path="transferView" element={<TransferView />} />
+>>>>>>> 3178d39 (chore: 충동해결(#14))
             <Route
               path="retirement-management"
               element={<RetirementManagement />}
@@ -63,7 +81,10 @@ function App() {
       </Router>
     </RecoilRoot>
   );
+<<<<<<< HEAD
 
+=======
+>>>>>>> 3178d39 (chore: 충동해결(#14))
 }
 
 export default App;
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,16 @@ export { default as inheritance } from "./inheritance/Inheritance";
 export { default as RetirementManagement } from "./retirement-management/RetirementManagement";
 export { default as Saving } from "./saving/Saving";
 export { default as SimpleTransaction } from "./simple-transaction/SimpleTransaction";
+<<<<<<< HEAD
 export { default as AutoTransfer } from "./simple-transaction/AutoTransfer";
 export { default as TransferView } from "./simple-transaction/TransferView";
+<<<<<<< HEAD
 export { default as TransactionHistory } from "./simple-transaction/TransactionHistory/TransactionHistory";
+=======
+=======
+export { default as TransactionHistory } from "./simple-transaction/TransactionHistory/TransactionHistory";
+>>>>>>> 799647e (chore: 인덱스생성(#14))
+>>>>>>> 3178d39 (chore: 충동해결(#14))
 export { default as Login } from "./login/Login";
 export { default as Home } from "./home/Home";
 export { default as Identify } from "./signup/Identify";
