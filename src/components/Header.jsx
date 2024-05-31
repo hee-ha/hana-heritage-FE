8f7fa9fbe1e0172cc98d709054e123e7c30cf0a8
JeongHeum Byun <quswjdgma83@gmail.com>
2024-05-31 17:16:33 +0900

    style: Nav, Header 변경

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header className="bg-white shadow  shadow-hanaGreen">
-      <div className=" mx-auto justify-between items-center px-24 py-6">
+    <header className="bg-white shadow shadow-hanaGreen">
+      <div className="mx-auto flex justify-between items-center px-24 py-6">
         <div className="flex flex-row items-center">
           <Link to="/">
             <img
@@ -16,13 +16,15 @@ const Header = () => {
           </Link>
           <Nav />
         </div>
-        {/* <Link
-          to="/login"
-          className="text-white bg-hanaRed  font-hana2 font-medium rounded-md px-3 py-2 text-3xl"
-          aria-current="page"
-        >
-          로그인
-        </Link> */}
+        <div>
+          <Link
+            to="/logout"
+            className="text-white bg-hanaRed font-hana2 font-semibold rounded-md px-3 py-2 text-4xl"
+            aria-current="page"
+          >
+            로그아웃
+          </Link>
+        </div>
       </div>
     </header>
   );
