0438aea18efae46abba899f1d99f4ba9bcc69454
hyerimmy <hyerim2000@swu.ac.kr>
2024-06-03 03:23:29 +0900

    feat : 예적금 상품 상세조회 api 연결 및 값 사용

--- src/pages/deposits/DepositsDetail.jsx
+++ src/pages/deposits/DepositsDetail.jsx
@@ -1,42 +1,72 @@
-import { React, useState } from "react";
-import { Accordion } from "flowbite-react";
-import { Link } from "react-router-dom";
+import { React, useEffect, useState } from "react";
+import { Link, useSearchParams } from "react-router-dom";
+import axios from "axios";
 
 function DepositsDetail() {
+  const [depositsDetail, setDepositsDetail] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+  console.log(searchParams.get("id"));
+  
+  useEffect(() => {
+    // TODO : url, token 모듈에서 사용하도록 하드코딩 제거
+    let url = "http://127.0.0.1:80/api/v1/deposits-products/detail?id="+searchParams.get("id");
+    console.log(url);
+
+    axios
+      .get(url, {headers: {
+        Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJyb2xlIjoiVVNFUiIsImlkIjoxLCJpYXQiOjE3MTczNDE2MzUsImV4cCI6MTcxNzQyODAzNX0.Q78FMHH0YQazLrK_29Lpu94xjms2gk2E2Ngp6zUqNT0`
+    }})
+      .then((res) => {
+        console.log("받아온 데이터: ", res.data.result);
+        setDepositsDetail(res.data.result);
+      })
+      .catch((error) => {
+        console.log("에러: ", error);
+      });
+  }, []);
+  
   return (
     <div className="px-24 font-noto text-3xl">
       <header>
         <h2 className="font-hana2 font-semibold text-6xl py-10">
-          선택하신 <span className="text-hanaGreen">펫사랑 적금</span>{" "}
+          선택하신 <span className="text-hanaGreen">{depositsDetail.finPrdtNm}</span>{" "}
           안내드립니다.
         </h2>
         <hr />
       </header>
       <div className="py-10">
-        <p className="font-hana2 font-semibold text-5xl pb-5">펫사랑 적금</p>
-        <p className="font-hana2">우리 가족 댕냥이의 두근두근 첫 재테크 !!!</p>
+        <p className="font-hana2 font-semibold text-5xl pb-5">{depositsDetail.finPrdtNm}</p>
+        <p className="font-hana2">{depositsDetail.finPrdtNm}으로 목돈 모아보세요!</p>
 
         <div className="grid grid-cols-2 gap-5 my-10">
           <div className="bg-hanaGold rounded-lg p-10 text-white">
-            <p className="mb-5 font-hana2">특징</p>
-            <p className="text-5xl font-bold mb-5">보험서비스 무료가입</p>
-            <p className="text-5xl font-bold">사랑의 서약서</p>
+            <p className="mb-5 font-hana2">종류</p>
+            <p className="text-5xl font-bold mb-5 leading-snug">
+              {depositsDetail.type}
+            </p>
           </div>
-
           <div className="bg-hanaGold rounded-lg p-10 text-white">
-            <p className="mb-5 font-hana2"> 최고 가입한도</p>
-            <p className="text-5xl font-bold mb-5">월 50만원</p>
+            <p className="mb-5 font-hana2">가입 대상</p>
+            <p className="text-5xl font-bold mb-5 leading-snug">
+              {(depositsDetail.joinMember||"").split('\n').map((line, index) => (
+                <span key={index}>
+                  {line}
+                  <br />
+                </span>
+              ))}
+            </p>
           </div>
-
           <div className="bg-hanaGold rounded-lg p-10 text-white">
-            <p className="mb-5 font-hana2">최고 가입한도</p>
-            <p className="text-5xl font-bold mb-5">보험서비스 무료가입</p>
+            <p className="mb-5 font-hana2">가입 제한</p>
+            <p className="text-5xl font-bold mb-5 leading-snug">
+              {depositsDetail.joinDeny}개까지 가입 가능
+            </p>
           </div>
-
           <div className="bg-hanaGold rounded-lg p-10 text-white">
-            <p className="mb-5 font-hana2">금리 (2023.11.13 기준, 세전)</p>
-            <p className="text-5xl font-bold mb-5">기본 2.30%</p>
-            <p className="text-5xl font-bold mb-5">최고 2.80%</p>
+            <p className="mb-5 font-hana2">최고 한도</p>
+            <p className="text-5xl font-bold mb-5 leading-snug">
+              {depositsDetail.maxLimit}만원
+            </p>
           </div>
         </div>
 
@@ -52,55 +82,44 @@ function DepositsDetail() {
         <ul role="list" class="divide-y divide-hanaSilver">
           <li className="justify-between gap-x-6 py-5">
             <p className="my-5 font-hana2 font-bold text-hanaGreen">
-              01. 상품 특징
+              01. 만기 후 이자율
             </p>
             <p className="text-5xl mb-5 leading-snug">
-              펫코노미 시대(반려인 1,500만명) 반려동물을 위한 목돈마련 저축상품
+              {(depositsDetail.mtrtInt||"").split('\n').map((line, index) => (
+                  <span key={index}>
+                    {line}
+                    <br />
+                  </span>
+              ))}
             </p>
           </li>
 
           <li className="justify-between gap-x-6 py-5">
             <p className="my-5 font-hana2 font-bold text-hanaGreen">
-              02. 가입대상
+              02. 우대 조건
             </p>
             <p className="text-5xl mb-5 leading-snug">
-              실명의 개인 또는 개인사업자 (1인 1계좌)
+              {(depositsDetail.spclCnd||"").split('\n').map((line, index) => (
+                  <span key={index}>
+                    {line}
+                    <br />
+                  </span>
+              ))}
             </p>
           </li>
 
           <li className="justify-between gap-x-6 py-5">
             <p className="my-5 font-hana2 font-bold text-hanaGreen">
-              03. 예금종류
-            </p>
-            <p className="text-5xl mb-5 leading-snug">적립식예금(정액적립식)</p>
-          </li>
-
-          <li className="justify-between gap-x-6 py-5">
-            <p className="my-5 font-hana2 font-bold text-hanaGreen">
-              04. 가입기간
-            </p>
-            <p className="text-5xl mb-5 leading-snug">1년</p>
-          </li>
-
-          <li className="justify-between gap-x-6 py-5">
-            <p className="my-5 font-hana2 font-bold text-hanaGreen">
-              05. 가입금액
-            </p>
-            <p className="text-5xl mb-5 leading-snug text-hanaRed">
-              월 10만원 ~ 50만원 (원단위)
-            </p>
-          </li>
-          <li className="justify-between gap-x-6 py-5">
-            <p className="my-5 font-hana2 font-bold text-hanaGreen">
-              06. 이자지급방법
+              03. 기타 유의사항
             </p>
             <p className="text-5xl mb-5 leading-snug">
-              만기일시지급식 : 만기(후)해지시 이자를 지급
+              {(depositsDetail.etcNote||"").split('\n').map((line, index) => (
+                  <span key={index}>
+                    {line}
+                    <br />
+                  </span>
+              ))}
             </p>
-          </li>
-          <li className="justify-between gap-x-6 py-5">
-            <p className="my-5 font-hana2 font-bold text-hanaGreen">07. 금리</p>
-            <p className="text-5xl mb-5 leading-snug">2.3%</p>
           </li>
         </ul>
 
