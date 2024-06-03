5cbaa1351eccd4388d8b1fd3d48d07ed642128f6
lee010207 <jy0260@gmail.com>
2024-06-03 10:50:48 +0900

    feat : 계좌이체 자동이체 api 연결

--- src/pages/transfer/TransferView.jsx
+++ src/pages/transfer/TransferView.jsx
@@ -1,7 +1,40 @@
+import axios from "axios";
 import React from "react";
-import { Link, useLocation } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 function TransferView() {
+  const location = useLocation();
+  const formData = location.state?.formData || {};
+  const type = location.state?.type || "";
+  const accountNumber = location.state?.selectedAccount || "";
+console.log(formData);
+console.log(accountNumber);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let url = "";
+    if (type === 'simple') {
+      url = "http://localhost/api/v1/transfer/simple";
+    } else if (type === 'auto') {
+      url = "http://localhost/api/v1/autoTransfer/register";
+    }
+
+    const formDataToSend = {
+      ...formData,
+      amount: parseInt(formData.amount.replace(/,/g, ""))
+    };
+    console.log(formDataToSend);
+    axios
+      .post(url, formDataToSend)
+      .then((res) => {
+        console.log("전송 성공: ", res.data);
+        // 성공 시 처리 로직 추가
+      })
+      .catch((error) => {
+        console.log("전송 에러: ", error);
+        // 실패 시 처리 로직 추가
+      });
+  };
+
   return (
     <div className="px-24">
       <header className="sectionHead">
@@ -12,47 +45,71 @@ function TransferView() {
       </header>
       <main className="flex flex-col items-center flex-grow p-4">
         <div className="text-5xl font-noto font-semibold mb-10">
-          <span className="text-hanaGreen">국민은행</span>{" "}
-          <span className="text-hanaGreen">김하나</span>
-          님께 <sapn className="text-hanaGreen">1</sapn>원을 이체합니다.
+          <span className="text-hanaGreen">{formData.recipientBank}은행</span>{" "}
+          <span className="text-hanaGreen">{formData.recipientRemarks}</span>
+          님께 <span className="text-hanaGreen">{formData.amount}</span>원을 이체합니다.
         </div>
-        <div className="bg-white font-noto text-3xl rounded-lg shadow-lg p-6 mt-6 w-full">
-          <div className="space-y-8">
-            <div className="flex justify-between">
-              <span className="font-hana2 text-gray-400 text-4xl">
-                입금계좌
-              </span>
-              <span>
-                국민은행 <span> 93800200281364</span>
-              </span>
-            </div>
-            <div className="flex justify-between">
-              <span className="font-hana2 text-gray-400 text-4xl">
-                출금계좌
-              </span>
-              <span>Young하나통장 113-910374-06707</span>
-            </div>
-            <div className="flex justify-between">
-              <span className="font-hana2 text-gray-400 text-4xl">수수료</span>
-              <span>면제</span>
-            </div>
-            <div className="flex justify-between">
-              <span className="font-hana2 text-gray-400 text-4xl">
-                받는분에게 표기
-              </span>
-              <span>변정흠</span>
-            </div>
-            <div className="flex justify-between">
-              <span className="font-hana2 text-4xl text-gray-400">
-                나에게 표기
-              </span>
-              <span>변정흠</span>
+        <form onSubmit={handleSubmit} className="w-full">
+          <div className="bg-white font-noto text-3xl rounded-lg shadow-lg p-6 mt-6">
+            <div className="space-y-8">
+              <div className="flex justify-between">
+                <span className="font-hana2 text-gray-400 text-4xl">
+                  출금계좌
+                </span>
+                <span>{accountNumber}</span>
+              </div>
+              <div className="flex justify-between">
+                <span className="font-hana2 text-gray-400 text-4xl">
+                  입금계좌
+                </span>
+                <span>{formData.recipientNumber}</span>
+                {type === 'auto' && (
+                <span>{formData.toAccountNumber}</span>
+              )}
+              </div>
+              <div className="flex justify-between">
+                <span className="font-hana2 text-gray-400 text-4xl">이체금액</span>
+                <span>{formData.amount}원</span>
+              </div>
+              <div className="flex justify-between">
+                <span className="font-hana2 text-gray-400 text-4xl">수수료</span>
+                <span>면제</span>
+              </div>
+              <div className="flex justify-between">
+                <span className="font-hana2 text-gray-400 text-4xl">
+                  받는분에게 표기
+                </span>
+                <span>{formData.recipientRemarks}</span>
+              </div>
+              <div className="flex justify-between">
+                <span className="font-hana2 text-4xl text-gray-400">
+                  나에게 표기
+                </span>
+                <span>{formData.senderRemarks}</span>
+              </div>
+             
+              {type === 'auto' && (
+                <>
+                  <div className="flex justify-between">
+                    <span className="font-hana2 text-gray-400 text-4xl">
+                      자동이체 일자
+                    </span>
+                    <span>매달 {formData.autoTransferDay}일</span>
+                  </div>
+                  <div className="flex justify-between">
+                    <span className="font-hana2 text-gray-400 text-4xl">
+                      자동이체 기간
+                    </span>
+                    <span>{formData.startDate} ~ {formData.endDate}</span>
+                  </div>
+                </>
+              )}
             </div>
           </div>
-        </div>
-        <button className="w-full text-white font-hana2 font-semibold text-5xl bg-hanaRed py-3 px-8 z-10 mt-4 transition-transform transform hover:animate-bubbly rounded-lg">
-          김하나님께 이체
-        </button>
+          <button className="w-full text-white font-hana2 font-semibold text-5xl bg-hanaRed py-3 px-8 z-10 mt-4 transition-transform transform hover:animate-bubbly rounded-lg">
+            이체하기
+          </button>
+        </form>
       </main>
     </div>
   );
