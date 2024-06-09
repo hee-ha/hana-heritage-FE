3d6fa3fd67f5598bc790cf801f51e9085bc20c66
JeongHeum Byun <quswjdgma83@gmail.com>
2024-06-10 02:03:28 +0900

    feat: 계좌개설 페이지들에 OCR 붙이기

--- src/pages/account/AccountCreation3.jsx
+++ src/pages/account/AccountCreation3.jsx
@@ -2,6 +2,7 @@ import { React, useState } from "react";
 import { Link } from "react-router-dom";
 import { useLocation, useNavigate } from "react-router-dom";
 import { createAccount } from "../../apis/account/createAccount";
+import OcrAuthenticationModal from "../inheritance/OcrAuthentication";
 
 function AccountCreation3() {
   const location = useLocation();
@@ -9,12 +10,26 @@ function AccountCreation3() {
   const [userId, setUserId] = useState(location.state?.userId);
 
   const [showPassword, setShowPassword] = useState(false);
+  const [showModal, setShowModal] = useState(false); // 모달 상태 추가
+  const [ocrData, setOcrData] = useState(null); // OCR 데이터 상태 추가
   const navigate = useNavigate();
+
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
   };
 
   const handleCreateButton = () => {
+    setShowModal(true); // 개설 버튼 클릭 시 모달을 띄움
+  };
+
+  const handleCloseModal = () => setShowModal(false);
+  const handleSaveModal = (data) => {
+    setOcrData(data);
+    setShowModal(false);
+    createAccountRequest(); // OCR 인증 후 계좌 생성 요청
+  };
+
+  const createAccountRequest = () => {
     let accountInfo = {
       accountName: formData.accountName,
       accountPassword: formData.password,
@@ -182,6 +197,12 @@ function AccountCreation3() {
           </button>
         </Link>
       </div>
+
+      <OcrAuthenticationModal
+        show={showModal}
+        handleClose={handleCloseModal}
+        handleSave={handleSaveModal}
+      />
     </div>
   );
 }
